3ddcc48dce819a9f191e388b6d3cd62f6d1795a7
Chunqi Shi <chqshi@gmail.com>
2015-04-01 23:24:01 +0100

    fnished

--- src/main/java/edu/brandeis/cs/paper/Compare.java
+++ src/main/java/edu/brandeis/cs/paper/Compare.java
@@ -1,5 +1,13 @@
 package edu.brandeis.cs.paper;
 
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpNameFinder;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import edu.brandeis.cs.uima.AnnotatorPipeline;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.pipeline.XMLOutputter;
 import gate.*;
@@ -19,6 +27,8 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.Feature;
 import org.apache.uima.cas.Type;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.util.XmlCasSerializer;
 import org.json.JSONObject;
@@ -32,6 +42,7 @@ import java.util.Properties;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
 import static org.apache.uima.fit.factory.ExternalResourceFactory.createDependencyAndBind;
 import static org.apache.uima.fit.factory.JCasFactory.createJCasFromPath;
 
@@ -41,6 +52,36 @@ import static org.apache.uima.fit.factory.JCasFactory.createJCasFromPath;
  *  2.
  */
 public class Compare {
+
+
+
+    public static void uimaStanford() throws Exception{
+        CollectionReader reader = createReader(
+                TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, "src/main/resources",
+                TextReader.PARAM_LANGUAGE, "en",
+                TextReader.PARAM_PATTERNS, new String[] { "[+]*.txt" });
+        AnalysisEngineDescription seg = createEngineDescription(StanfordSegmenter.class);
+        AnalysisEngineDescription tagger = createEngineDescription(StanfordPosTagger.class);
+        AnalysisEngineDescription ner = createEngineDescription(StanfordNamedEntityRecognizer.class);
+//        AnalysisEngineDescription parser = createEngineDescription(StanfordParser.class);
+        AnalysisEngineDescription writer = createEngineDescription(AnnotatorPipeline.NPNEWriter.class);
+        SimplePipeline.runPipeline(reader, seg, tagger, ner, writer);
+    }
+
+    public static void uimaOpennlp() throws Exception {
+        CollectionReader reader = createReader(
+                TextReader.class,
+                TextReader.PARAM_SOURCE_LOCATION, "src/main/resources",
+                TextReader.PARAM_LANGUAGE, "en",
+                TextReader.PARAM_PATTERNS, new String[] { "[+]*.txt" });
+        AnalysisEngineDescription seg = createEngineDescription(OpenNlpSegmenter.class);
+        AnalysisEngineDescription tag = createEngineDescription(OpenNlpPosTagger.class);
+        AnalysisEngineDescription ner = createEngineDescription(OpenNlpNameFinder.class);
+        AnalysisEngineDescription writer = createEngineDescription(AnnotatorPipeline.NPNEWriter.class);
+        SimplePipeline.runPipeline(reader, seg, tag, ner, writer);
+    }
+
     public static void gateInit() throws Exception {
         Out.prln("Initialising GATE...");
         File gatedir = FileUtils.toFile(Compare.class.getResource("/gate/"));
--- src/main/java/edu/brandeis/cs/service/IPipelineAnnotation.java
+++ src/main/java/edu/brandeis/cs/service/IPipelineAnnotation.java
@@ -4,6 +4,6 @@ package edu.brandeis.cs.service;
  * Created by lapps on 4/1/2015.
  */
 public interface IPipelineAnnotation {
-    public String getXML(String doc);
-    public String getJSON(String doc);
+    public String getXML(String doc) throws Exception;
+    public String getJSON(String doc) throws Exception;
 }
